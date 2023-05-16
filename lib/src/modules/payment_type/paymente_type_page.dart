793c4341ea3d405e799b13e57ece235dfb0fa783
Paulo Miranda <paulo.miranda@customtech.com.br>
2023-05-16 10:35:47 -0300

    -Código dart week

---
+++ lib/src/modules/payment_type/paymente_type_page.dart
@@ -0,0 +1,139 @@
+import 'package:delivery_backoffice_dw10/src/modules/payment_type/widgets/paymentTypeForm/payment_type_form_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../models/payment_type_model.dart';
+import 'payment_type_controller.dart';
+import 'widgets/payment_type_header.dart';
+import 'widgets/payment_type_item.dart';
+
+class PaymenteTypePage extends StatefulWidget {
+  const PaymenteTypePage({super.key});
+
+  @override
+  State<PaymenteTypePage> createState() => _PaymenteTypePageState();
+}
+
+class _PaymenteTypePageState extends State<PaymenteTypePage>
+    with Loader, Messages {
+  final controller = Modular.get<PaymentTypeController>();
+
+  final disposers = <ReactionDisposer>[];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final filterDisposer = reaction(
+        (_) => controller.filterEnabled,
+        (_) {
+          controller.loadPayments();
+        },
+      );
+
+      final statusDisposer = reaction((_) => controller.status, (status) {
+        switch (status) {
+          case PaymentTypeStateStatus.initial:
+            break;
+          case PaymentTypeStateStatus.loading:
+            showLoader();
+            break;
+          case PaymentTypeStateStatus.laoded:
+            hideloader();
+            break;
+          case PaymentTypeStateStatus.error:
+            hideloader();
+            showError(
+              controller.errorMessage ?? 'Erro ao buscar formas de pagamentos',
+            );
+            break;
+          case PaymentTypeStateStatus.addOrUpdatePayment:
+            hideloader();
+            showAddOrUpdatePayment();
+            break;
+          case PaymentTypeStateStatus.saved:
+            hideloader();
+            Navigator.of(context, rootNavigator: true).pop();
+            controller.loadPayments();
+            break;
+        }
+      });
+      disposers.addAll([statusDisposer, filterDisposer]);
+      controller.loadPayments();
+    });
+  }
+
+  @override
+  void dispose() {
+    for (final dispose in disposers) {
+      dispose();
+    }
+    super.dispose();
+  }
+
+  void showAddOrUpdatePayment() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Material(
+          color: Colors.black26,
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            backgroundColor: Colors.white,
+            elevation: 10,
+            child: PaymentTypeFormModal(
+              model: controller.paymentTypeSelected,
+              controller: controller,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[50],
+      padding: const EdgeInsets.only(left: 40, top: 40, right: 40),
+      child: Column(
+        children: [
+          PaymentTypeHeader(
+            controller: controller,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Expanded(
+            child: Observer(
+              builder: (_) {
+                return GridView.builder(
+                  itemCount: controller.paymentTypes.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    mainAxisExtent: 120,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 10,
+                    maxCrossAxisExtent: 680,
+                  ),
+                  itemBuilder: (context, index) {
+                    final paymentTypeModel = controller.paymentTypes[index];
+                    return PaymentTypeItem(
+                      payment: paymentTypeModel,
+                      controller: controller,
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
